d42f8ce3d99980acd0ae5cf23d1cf05c9a686b69
Evandro Capovilla <vandinhocapovilla@live.com>
2018-10-08 16:59:03 -0300

    Initial Commit

---
+++ .cryptomodule.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/richbitch/Desktop/git/crypto-device-kernel/cryptomodule.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/richbitch/Desktop/git/crypto-device-kernel/cryptomodule.ko /home/richbitch/Desktop/git/crypto-device-kernel/cryptomodule.o /home/richbitch/Desktop/git/crypto-device-kernel/cryptomodule.mod.o ;  true
---
+++ .tmp_versions/cryptomodule.mod
@@ -0,0 +1,3 @@
+/home/richbitch/Desktop/git/crypto-device-kernel/cryptomodule.ko
+/home/richbitch/Desktop/git/crypto-device-kernel/cryptomodule.o
+
