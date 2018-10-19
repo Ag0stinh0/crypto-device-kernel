e0bcf18d4cf2d7552aa12168610dffe5eae7ec2a
unknown <t.sanches.araujo@gmail.com>
2018-10-18 22:49:18 -0300

    Teste HASH

--- cryptomodule.c
+++ cryptomodule.c
@@ -1,59 +1,89 @@
 #include <linux/init.h> // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h> // Core header for loading LKMs into the kernel   
+#include <linux/module.h> // Core header for loading LKMs into the kernel
 #include <linux/device.h> // Header to support the kernel Driver Model
 #include <linux/crypto.h>
 #include <linux/kernel.h>  // Contains types, macros, functions for the kernel
 #include <linux/fs.h>      // Header for the Linux file system support
 #include <linux/uaccess.h> // Required for the copy to user function
 #include <linux/scatterlist.h>
+#include <crypto/internal/hash.h>
 
 #define DEVICE_NAME "crypto" ///< The device will appear at /dev/ebbchar using this value
-#define CLASS_NAME "cpt"     ///< The device class -- this is a character device driver
+#define CLASS_NAME  "cpt"     ///< The device class -- this is a character device driver
+
+#define HASH_LENGTH (256/8) ///< length of hash256
 
 MODULE_LICENSE("GPL");                                                ///< The license type -- this affects available functionality
 MODULE_AUTHOR("Evandro Agostinho Pedro Lucas Brunno");                                  ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("Modulo de Linux para cryptografar uma mensagem"); ///< The description -- see modinfo
-MODULE_VERSION("0.1");   
+MODULE_DESCRIPTION("Modulo de Linux para criptografar uma mensagem"); ///< The description -- see modinfo
+MODULE_VERSION("0.1");
 
-static char *key = "0123456789ABCDEF";
+static char *key;
 
 static int majorNumber;                     ///< Stores the device number -- determined automatically
-static char message[256] = {0};             ///< Memory for the string that is passed from userspace
+static char message[256 * 2 + 1];             ///< Memory for the string that is passed from userspace (plaintext)
 static short size_of_message;               ///< Used to remember the size of the string stored
 static int numberOpens = 0;                 ///< Counts the number of times the device is opened
 static struct class *cryptoClass = NULL;   ///< The device-driver class struct pointer
 static struct device *cryptoDevice = NULL; ///< The device-driver device struct pointer
 
-//receber por parametros
-
+// Parameters
 module_param(key, charp, 0000);
-MODULE_PARM_DESC(key, "Chave para cryptografia");
+MODULE_PARM_DESC(key, "Chave para criptografia");
 
-// The prototype functions for the character driver -- must come before the struct definition
+// The prototype functions for the character driver
 static int dev_open(struct inode *, struct file *);
 static int dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
-/** @brief Devices are represented as file structure in the kernel. The file_operations structure from
- *  /linux/fs.h lists the callback functions that you wish to associated with your file operations
- *  using a C99 syntax structure. char devices usually implement open, read, write and release calls
- */
+// File Struct
 static struct file_operations fops =
-    {
-        .open = dev_open,
-        .read = dev_read,
-        .write = dev_write,
-        .release = dev_release,
+{
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
 };
 
-/** @brief The LKM initialization function
- *  The static keyword restricts the visibility of the function to within this C file. The __init
- *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
- *  time and that it can be discarded and its memory freed up after that point.
- *  @return returns 0 if successful
- */
+// Hash Functions
+static void show_hash(char *hash_text){
+    int i;
+    char str[HASH_LENGTH * 2 + 1];
 
+    for(i = 0; i < HASH_LENGTH; i++)
+        sprintf(&str[i*2], "%02x", (unsigned char)hash_text[i]);
+    str[i*2] = 0;
+    pr_info("%s\n", str);
+}
+
+void hash_create(void){
+    char hash_sha256[HASH_LENGTH];
+    struct crypto_shash *sha256;
+    struct shash_desc *shash;
+
+    sha256 = crypto_alloc_shash("sha256", 0, 0);
+
+    if( IS_ERR(sha256) ) return -1
+
+    shash = kmalloc(sizeof(struct shash_desc) + crypto_shash_descsize(sha256), GFP_KERNEL);
+
+    if( !shash ) return -ENOMEM;
+
+    shash->tfm = sha256;
+    shash->flags = 0;
+
+    if( crypto_shash_init(shash) ) return -1;
+    if( crypto_shash_update(shash, message, strlen(plaintext)) ) return -1;
+    if( crypto_shash_final(shash, hash_sha256) ) return -1;
+
+    kfree(shash);
+    crypto_free_shash(sha256);
+
+    show_hash(hash_sha256);
+}
+
+// Module init
 static int __init crypto_init(void)
 {
     majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
@@ -62,14 +92,14 @@ static int __init crypto_init(void)
         return majorNumber;
     }
 
-    cryptoClass = class_create(THIS_MODULE, DEVICE_NAME); //Registra a classe do device
+    cryptoClass = class_create(THIS_MODULE, DEVICE_NAME); // Class creation
     if (IS_ERR(cryptoClass)){                // Check for error and clean up if there is
       unregister_chrdev(majorNumber, DEVICE_NAME);
       printk(KERN_ALERT "Failed to register device class\n");
       return PTR_ERR(cryptoClass);          // Correct way to return an error on a pointer
     }
 
-    cryptoDevice = device_create(cryptoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME); //Registra a device do driver
+    cryptoDevice = device_create(cryptoClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME); // Device Driver creation
     if (IS_ERR(cryptoDevice)){               // Clean up if there is an error
         class_destroy(cryptoClass);           // Repeated code but the alternative is goto statements
         unregister_chrdev(majorNumber, DEVICE_NAME);
@@ -77,6 +107,7 @@ static int __init crypto_init(void)
         return PTR_ERR(cryptoDevice);
     }
 
+    hash_create();
     printk(KERN_INFO "CryptoModule: modulo crypto inicializado com a chave: %s.\n", key);
     return 0;
 }
@@ -100,7 +131,7 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
    int error_count = 0;
    // copy_to_user has the format ( * to, *from, size) and returns 0 on success
    error_count = copy_to_user(buffer, message, size_of_message);
- 
+
    if (error_count==0){            // if true then have success
       printk(KERN_INFO "CryptoModule: Enviou %d caracteres para o usuario\n", size_of_message);
       return (size_of_message=0);  // clear the position to the start and return 0
