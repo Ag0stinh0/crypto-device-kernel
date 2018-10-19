1f75ff93691cbfa8ad396551588b9c4517923f99
unknown <t.sanches.araujo@gmail.com>
2018-10-18 23:13:27 -0300

    Teste AES

--- cryptomodule.c
+++ cryptomodule.c
@@ -7,11 +7,14 @@
 #include <linux/uaccess.h> // Required for the copy to user function
 #include <linux/scatterlist.h>
 #include <crypto/internal/hash.h>
+#include <crypto/internal/skcipher.h>
 
 #define DEVICE_NAME "crypto" ///< The device will appear at /dev/ebbchar using this value
 #define CLASS_NAME  "cpt"     ///< The device class -- this is a character device driver
 
 #define HASH_LENGTH (256/8) ///< length of hash256
+#define SYMMETRIC_KEY_LENGTH 32
+#define CIPHER_BLOCK_SIZE 16
 
 MODULE_LICENSE("GPL");                                                ///< The license type -- this affects available functionality
 MODULE_AUTHOR("Evandro Agostinho Pedro Lucas Brunno");                                  ///< The author -- visible when you use modinfo
@@ -45,6 +48,148 @@ static struct file_operations fops =
     .write = dev_write,
     .release = dev_release,
 };
+
+// AES Struct
+struct tcrypt_result {
+    struct completion completion;
+    int err;
+};
+
+struct skcipher_def {
+    struct scatterlist sg;
+    struct crypto_skcipher * tfm;
+    struct skcipher_request * req;
+    struct tcrypt_result result;
+    char * scratchpad; // Palavra a ser encriptada
+    char * ciphertext;
+    char * ivdata;
+};
+static struct skcipher_def sk;
+
+// AES Encryption
+static void test_skcipher_finish(struct skcipher_def * sk)
+{
+    if (sk->tfm)
+        crypto_free_skcipher(sk->tfm);
+    if (sk->req)
+        skcipher_request_free(sk->req);
+    if (sk->ivdata)
+        kfree(sk->ivdata);
+    if (sk->scratchpad)
+        kfree(sk->scratchpad);
+    if (sk->ciphertext)
+        kfree(sk->ciphertext);
+}
+
+static int test_skcipher_result(struct skcipher_def * sk, int rc)
+{
+    switch (rc) {
+        case 0:
+            break;
+        case -EINPROGRESS:
+        case -EBUSY:
+            rc = wait_for_completion_interruptible(&sk->result.completion);
+            if (!rc && !sk->result.err) {
+                reinit_completion(&sk->result.completion);
+                break;
+            }
+        default:
+            pr_info("skcipher encrypt returned with %d result %d\n", rc, sk->result.err);
+            break;
+    }
+    init_completion(&sk->result.completion);
+    return rc;
+}
+
+static void test_skcipher_callback(struct crypto_async_request *req, int error)
+{
+    struct tcrypt_result *result = req->data;
+    int ret;
+    if (error == -EINPROGRESS)
+    return;
+    result->err = error;
+    complete(&result->completion);
+    pr_info("Encryption finished successfully\n");
+}
+
+static int test_skcipher_encrypt(char * plaintext, char * password, struct skcipher_def * sk)
+{
+    int ret = -EFAULT;
+    unsigned char key[SYMMETRIC_KEY_LENGTH];
+    if (!sk->tfm) {
+        sk->tfm = crypto_alloc_skcipher("cbc-aes-aesni", 0, 0);
+        if (IS_ERR(sk->tfm)) {
+            pr_info("could not allocate skcipher handle\n");
+            return PTR_ERR(sk->tfm);
+        }
+    }
+    if (!sk->req) {
+        sk->req = skcipher_request_alloc(sk->tfm, GFP_KERNEL);
+        if (!sk->req) {
+            pr_info("could not allocate skcipher request\n");
+            ret = -ENOMEM;
+            goto out;
+        }
+    }
+    skcipher_request_set_callback(sk->req, CRYPTO_TFM_REQ_MAY_BACKLOG,
+    test_skcipher_callback,
+    &sk->result);
+
+    memset((void*)key,'\0',SYMMETRIC_KEY_LENGTH);
+
+
+    sprintf((char*)key,"%s",password);
+
+    /* AES 256 with given symmetric key */
+    if (crypto_skcipher_setkey(sk->tfm, key, SYMMETRIC_KEY_LENGTH)) {
+        pr_info("key could not be set\n");
+        ret = -EAGAIN;
+        goto out;
+    }
+    pr_info("Symmetric key: %s\n", key);   //Sujeito a remoção
+    pr_info("Plaintext: %s\n", plaintext); //Sujeito a remoção
+    if (!sk->ivdata) {
+
+        sk->ivdata = kmalloc(CIPHER_BLOCK_SIZE, GFP_KERNEL);
+        if (!sk->ivdata) {
+            pr_info("could not allocate ivdata\n");
+            goto out;
+        }
+        get_random_bytes(sk->ivdata, CIPHER_BLOCK_SIZE);
+    }
+    if (!sk->scratchpad) {
+        /* The text to be encrypted */
+        sk->scratchpad = kmalloc(CIPHER_BLOCK_SIZE, GFP_KERNEL);
+        if (!sk->scratchpad) {
+            pr_info("could not allocate scratchpad\n");
+            goto out;
+        }
+    }
+    sprintf((char*)sk->scratchpad,"%s",plaintext);
+    sg_init_one(&sk->sg, sk->scratchpad, CIPHER_BLOCK_SIZE);
+    skcipher_request_set_crypt(sk->req, &sk->sg, &sk->sg,CIPHER_BLOCK_SIZE, sk->ivdata);
+    init_completion(&sk->result.completion);
+    /* encrypt data */
+    ret = crypto_skcipher_encrypt(sk->req);
+    ret = test_skcipher_result(sk, ret);
+    if (ret)
+        goto out;
+    pr_info("Encryption request successful\n");
+    out:
+    return ret;
+}
+
+int cryptoapi_create(void)
+{
+    char *password = key; // Remover C_PASSWORD para key recebida via parametro
+    sk.tfm = NULL;
+    sk.req = NULL;
+    sk.scratchpad = NULL;
+    sk.ciphertext = NULL;
+    sk.ivdata = NULL;
+    test_skcipher_encrypt(message, password, &sk);
+
+}
 
 // Hash Functions
 static void show_hash(char *hash_text){
@@ -118,6 +263,7 @@ static void __exit crypto_exit(void)
     class_unregister(cryptoClass);                      // unregister the device class
     class_destroy(cryptoClass);                         // remove the device class
     unregister_chrdev(majorNumber, DEVICE_NAME);         // unregister the major number
+    test_skcipher_finish(&sk);
     printk(KERN_INFO "CryptoModule: modulo crypto encerrado com sucesso!\n");
 }
 
